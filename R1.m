a16e223aea117d3d464e350ba04600e4e5ebf056
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-09-04 15:41:15 +0200

    Added so that R1 needs to be stepped through. The tree plot now also shows what new changes have happened and what caused it.

--- R1.m
+++ R1.m
@@ -7,12 +7,16 @@ for i = 1:rounds
     while sum(tree) ~= N
         
         index = floor(N*rand(1)+1);
-%         while tree(index)
-%             index = floor(N*rand(1)+1);
-%             result(i) = result(i) + 1;
-%         end
+        while tree(index)
+            index = floor(N*rand(1)+1);
+            result(i) = result(i) + 1;
+        end
         
-        tree = mark(tree, index);
+        new_tree = mark(tree, index);
+        plot_tree(new_tree, tree, index);
+        tree = new_tree;
+%         pause(0.5);
+        waitforbuttonpress;
         result(i) = result(i) + 1;
     end
 end
--- plot_tree.m
+++ plot_tree.m
@@ -1,5 +1,8 @@
-function [ ] = Untitled( tree )
+function [ ] = plot_tree( tree, old_tree, new_index )
 %plot_tree Special function for plotting tree's using matlabs treeplot
+% old_tree and new_index are optional but will, if supplied, help
+% see how the tree is changing.
+
     N = numel(tree);
     matlab_tree = generate_tree(N);
     [x, y] = treelayout(matlab_tree);
@@ -12,6 +15,17 @@ function [ ] = Untitled( tree )
 
     ones_array = find(tree);
     plot(x(ones_array,1), y(ones_array,1), 'r.','MarkerSize', 20);
+    
+    if exist('old_tree')
+        % find nodes that have been modifed
+        new_ones_array = setdiff(ones_array, find(old_tree));
+        plot(x(new_ones_array,1), y(new_ones_array,1), 'b.','MarkerSize', 20);
+    end
+    
+    if exist('new_index')
+        % mark the node that started the cascade
+        plot(x(new_index,1), y(new_index,1), 'g.','MarkerSize', 20);
+    end
 
     hold off;
 end
