8f56f9d5ec11f0bb324910e836b6a6432006fd04
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-09-04 14:48:29 +0200

    Treeplot now fills all nodes. Getting there...

--- test_tree.m
+++ test_tree.m
@@ -1,9 +1,16 @@
 N = 7;
 
-tree = generate_tree(N);
-[x, y] = treelayout(tree);
+matlab_tree = generate_tree(N);
+[x, y] = treelayout(matlab_tree);
 
 x = x';
 y = y';
-name1 = cellstr(num2str((1:count)'));
-text(x(:,1), y(:,1), name1, 'VerticalAlignment','bottom','HorizontalAlignment','right')
+name1 = cellstr(num2str((1:N)'));
+
+treeplot(matlab_tree);
+hold on;
+% text(x(:,1), y(:,1), name1, 'VerticalAlignment','bottom','HorizontalAlignment','right')
+
+plot(x(:,1), y(:,1), 'r.','MarkerSize', 20);
+
+hold off;
