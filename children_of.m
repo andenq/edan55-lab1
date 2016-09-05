5cd2944af4d2c69add3b02c29ef95a6dcb9754fa
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-09-05 12:33:19 +0200

    Further work

--- children_of.m
+++ children_of.m
@@ -1,7 +1,8 @@
 function [ child1, child2 ] = children_of( parent )
 %PARENT_OF Return index of parent for this child
     
-    child1 = bitshift(parent, 1);
+    %child1 = bitshift(parent, 1);
+    child1 = parent*2;
     child2 = child1+1;
 end
 
