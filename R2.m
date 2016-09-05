898b99b416d16525c047548c152e075e24ce421e
Samuel Johansson <johansson.samuel@live.se>
2016-09-05 12:34:48 +0200

    Optimized R2

--- R2.m
+++ R2.m
@@ -4,6 +4,10 @@ for i = 1:rounds
     tree = zeros(1, N);
     sequence = knuth_shuffle(1:N);
     for s = sequence
+        if tree(s)
+           result(i) = result(i) + 1;
+           continue
+        end
         tree = mark(tree, s);
         result(i) = result(i) + 1;
         
